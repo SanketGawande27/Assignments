220a05d33d74b378a9c44df17d6e7dd16f23e762
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-07-24 14:49:53 +0530

    Adding try catch exception class in driver class

--- Queue/src/com/assignment/driver/Driver.java
+++ Queue/src/com/assignment/driver/Driver.java
@@ -1,35 +1,52 @@
 package com.assignment.driver;
 import java.util.Scanner;
 
+import com.assignment.customexceptions.MyException;
+import com.assignment.node.Node;
 import com.assignment.queue.service.impl.Myqueue;
 public class Driver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MyException {
 		Myqueue myq = new Myqueue();
-		
+        MyException ex = new MyException(null);
 		Scanner scanner = new Scanner(System.in);
 		while(true) {
-		System.out.println("*******************************************\nPress \n1 for insert \n2 for Delete \n3 for Display \n4 for Size \n ");
-		int choice = scanner.nextInt();
+		System.out.print("\n*******************************************\nPress \n1 for insert \n2 for Delete \n3 for Display \n4 for Size \n");
+	    int choice = scanner.nextInt();
 		switch(choice) {
+		case 1:
+			System.out.print("Enter new element :");
+			myq.insert();
+		    break;
 		
-		case 1:myq.insert();
-		break;
+		case 2:
+		try {
+			System.out.print("Deleted Element :"+myq.delete());
+		    }catch(MyException ex1) {
+			System.out.print(ex1.getMessage());
+		    }
+			break;
 		
-		case 2:myq.delete();
-		break;
-		
-		case 3:myq.display();
-		break;
-		
-		case 4:myq.size();
-		break;
+		case 3:
+			 try {
+			System.out.print("Elements in Queue :");
+			myq.display();			
+			}catch(MyException ex1) {
+				System.out.println(ex1.getMessage());
+			}
+			break;
+		    
+	        case 4:
+		   System.out.print("Total Size :"+myq.size());
+		   break;
 		
 		default:
-			System.out.println("Input in wrong...  Enter Correct Input.....! ");
+			System.out.println("Input in wrong... \nEnter Correct Input.....! ");
 		
 		 } 
+		
 		}
+
 	}
 
 	

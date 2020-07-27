f45a353f371b70ada4ef97274e0d6092f6b11ec7
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-07-27 17:38:35 +0530

    update Myqueueclass

--- Queue/src/com/assignment/queue/service/impl/Myqueue.java
+++ Queue/src/com/assignment/queue/service/impl/Myqueue.java
@@ -1,78 +1,76 @@
 package com.assignment.queue.service.impl;
+
 import java.util.Scanner;
 
-import com.assignment.customexceptions.MyException;
+import com.assignment.customexpection.MyException;
 import com.assignment.node.Node;
 
 public class Myqueue {
-   
-	 private Node rear;
-	 private Node front;
-	 private int size;	
-	 private Node ptr; 
-	 
-	 public Myqueue() {
-		 front = null;
-		 rear=null;
-		 size=0;
-	 }
-	 
-	 public boolean isEmpty() {
-		 boolean res = false;
-		 if (size == 0) {
-	         res = true;
-		 }
-		 return res;
-	 }
-	
-	 public void insert() {
-		 Scanner sc = new Scanner(System.in);
-		 int element = sc.nextInt();
-		 Node node = new Node(element);
-		 if(front == null) {
-			   rear = node;
-			   front = node;
-			   size++;
-		   }
-		   else {
-			   rear.setNext(node);
-			   rear = node;
-			   size++;
-			}
-	 }
-	 
-	
-	 public int delete() throws MyException {
-	     	  if(isEmpty())
-	    	  throw new MyException("Queue is Already Empty Not Been Deleted......\nPlease Insert Elements First ....");
-	    	  Node ptr = front;
-	    	  front = ptr.getNext();
-	    	  if(front==null)
-	    	     rear = null;
-	    	     size--;     
-		  return ptr.getData();  	   	
-         }
-	   
-	 public int size() {
-	    	  return size;
-	 }
-	    
-	    
+
+	private Node rear;
+	private Node front;
+	private int size;
+	private Node ptr;
+
+	public Myqueue() {
+		front = null;
+		rear = null;
+		size = 0;
+	}
+
+	public boolean isEmpty() {
+		boolean res = false;
+		if (size == 0) {
+			res = true;
+		}
+		return res;
+	}
+
+	public void insert() {
+		Scanner sc = new Scanner(System.in);
+		int element = sc.nextInt();
+		Node node = new Node(element);
+		if (front == null) {
+			rear = node;
+			front = node;
+			size++;
+		} else {
+			rear.setNext(node);
+			rear = node;
+			size++;
+
+		}
+	}
+
+	public int delete() throws MyException {
+		if (isEmpty())
+			throw new MyException("Queue is Already Empty Not Been Deleted......\nPlease Insert Elements First ....");
+		Node ptr = front;
+		front = ptr.getNext();
+		if (front == null)
+			rear = null;
+		size--;
+		return ptr.getData();
+	}
+
+	public int size() {
+		return size;
+	}
+
 	@Override
-	 public String toString() {
-	     	  String result = "";
-                  Node ptr = front;
-                  while(ptr != null){
-                  result += ptr.getData();
-                  if(ptr.getNext() != null){
-                     result += ", ";
-                   }
-                     ptr = ptr.getNext();
-                  }
-                  if(result=="")
-            	  return "Queue is Empty ...! ";
-            
-                  return "Elements in Queue " + result;
-         }
-	     
- }
+	public String toString() {
+		String result = "";
+		Node ptr = front;
+		while (ptr != null) {
+			result += ptr.getData();
+			if (ptr.getNext() != null) {
+				result += ", ";
+			}
+			ptr = ptr.getNext();
+		}
+		if (result == "")
+			return "Queue is Empty ...! ";
+
+		return "Elements in Queue " + result;
+	}
+}

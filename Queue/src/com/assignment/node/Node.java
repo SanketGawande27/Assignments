1fef0393497fe10f422e6cfcb148dca474f8dd3e
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-07-27 17:36:35 +0530

    update Nodeclass

--- Queue/src/com/assignment/node/Node.java
+++ Queue/src/com/assignment/node/Node.java
@@ -1,28 +1,32 @@
 package com.assignment.node;
+
 public class Node {
-   private int data;
-   private Node next;
-   
-public Node(int data) {
-	this.data=data;
-	next = null;
-  }
+	private int data;
+	private Node next;
 
-public int getData() {
-	return data;
-  }
+	public Node(int data) {
+		this.data = data;
+		next = null;
+	}
 
-public void setData(int data) {
-	this.data = data;
-  }
+	public int getData() {
+		return data;
+	}
 
-public Node getNext() {
-	return next;
-  }
-public void setNext(Node next) {
-	this.next = next;
-  } 
-	
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [data=" + data + ", next=" + next + "]";
+	}
 }
-
-

c74ac910c2d73b08630d55898763a3f8d0ce3aff
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-07-27 15:05:42 +0530

    Update Nodeclass

--- Queue/src/com/assignment/node/Node.java
+++ Queue/src/com/assignment/node/Node.java
@@ -3,29 +3,25 @@ public class Node {
    private int data;
    private Node next;
    
-   public Node(int data) {
+public Node(int data) {
 	   this.data=data;
 	   next = null;
-   }
-	
-	public void dispaynode() {
-	System.out.print(data+" ");
-}
+  }
 
 public int getData() {
 	return data;
-}
+  }
 
 public void setData(int data) {
 	this.data = data;
-}
+  }
 
 public Node getNext() {
 	return next;
-}
+  }
 public void setNext(Node next) {
 	this.next = next;
-} 
+  } 
 	
 }
 

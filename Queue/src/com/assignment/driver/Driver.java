38d8367060b9475dc82cf9dd2364f25a2604ce1a
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-07-29 14:13:24 +0530

    Deiverclass for generic datatype

--- Queue/src/com/assignment/driver/Driver.java
+++ Queue/src/com/assignment/driver/Driver.java
@@ -7,23 +7,23 @@ import com.assignment.queue.service.impl.Myqueue;
 
 public class Driver {
 
-	public static void main(String[] args) throws MyException {
-		Myqueue myq = new Myqueue();
+	public static <E> void main(String[] args) throws MyException {
 		MyException ex = new MyException(null);
 		Scanner scanner = new Scanner(System.in);
+		Myqueue<E> myq = new Myqueue<E>();
 		while (true) {
 			System.out.print(
 					"\n*******************************************\nPress \n1 for insert \n2 for Delete \n3 for Display \n4 for Size \n");
 			int choice = scanner.nextInt();
 			switch (choice) {
 			case 1:
-				System.out.print("Enter new element :");
+				System.out.print("Enter new element: ");
 				myq.insert();
 				break;
 
 			case 2:
 				try {
-					System.out.print("Deleted Element :" + myq.delete());
+					System.out.print("Deleted Element: " + myq.delete());
 				} catch (MyException ex1) {
 					System.out.print(ex1.getMessage());
 				}

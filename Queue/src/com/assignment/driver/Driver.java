7e43476101b1c2f553435927251c0d97049c449b
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-08-01 22:56:11 +0530

    Adding Studentclass object

--- Queue/src/com/assignment/driver/Driver.java
+++ Queue/src/com/assignment/driver/Driver.java
@@ -4,63 +4,62 @@ import java.util.Scanner;
 
 import com.assignment.customexpection.MyException;
 import com.assignment.queue.service.impl.Myqueue;
+import com.assignment.studentinfo.StudentInfo;
 
 public class Driver {
 
 	public static <E> void main(String[] args) throws MyException {
 		MyException ex = new MyException(null);
-		StudentInfo<E> stud = new StudentInfo<E>();
-		Scanner scanner = new Scanner(System.in);
-		Myqueue<E> myq = new Myqueue<E>();
-		while (true) {
-			System.out.print(
-					"\n*******************************************\nPress \n1 for insert \n2 for Delete \n3 for Display \n4 for Size \n 5 for Insert StudentInfo");
-			int choice = scanner.nextInt();
-			switch (choice) {
-			case 1:
-				System.out.print("Enter new element: ");
-				String element = scanner.next();
-				myq.insert(element);
-				break;
+		Myqueue<StudentInfo> myq = new Myqueue<>();
 
-			case 2:
-				try {
-					System.out.print("Deleted Element: " + myq.delete());
-				} catch (MyException ex1) {
-					System.out.print(ex1.getMessage());
+		// StudentInfo<E> stud1 = new StudentInfo<E>();
+		try (Scanner sc = new Scanner(System.in)) {
+
+			while (true) {
+				System.out.print(
+						"\n*******************************************\nPress \n1 for insert \n2 for Delete \n3 for Display \n4 for Size \n5 for Insert Student \n");
+				int choice = sc.nextInt();
+				switch (choice) {
+				case 1:
+					System.out.print("Enter new element: ");
+					Integer element = sc.nextInt();
+					myq.insert(element);
+					break;
+
+				case 2:
+					try {
+						System.out.print("Deleted Element: " + myq.delete());
+					} catch (MyException ex1) {
+						System.out.print(ex1.getMessage());
+					}
+					break;
+
+				case 3:
+					System.out.println("Direct From Queue :");
+					System.out.println(myq);
+					break;
+
+				case 4:
+					System.out.print("Total Size :" + myq.size());
+					break;
+
+				case 5:
+					System.out.println("Enter Student Information : Name, Address, Rollno, Marks");
+					StudentInfo stud = new StudentInfo(sc.next(), sc.next(), sc.nextInt(), sc.nextDouble());
+					myq.insert(stud);
+					System.out.println("Entry Added Successfully");
+
+					break;
+
+				default:
+					System.out.println("Input in wrong... \nEnter Correct Input.....! ");
+
 				}
-				break;
-
-			case 3:
-				System.out.println(myq);
-				break;
-
-			case 4:
-				System.out.print("Total Size :" + myq.size());
-				break;
-					
-		       case 5:
-				System.out.println("Enter Student Information : Name, Rollno, Marks ,Address");
-				String name = scanner.next();
-				stud.setName(name);
-				int rollno = scanner.nextInt();
-				stud.setRollno(rollno);
-				float marks = scanner.nextFloat();
-				stud.setMarks(marks);
-				String address = scanner.next();
-				stud.setAddress(address);
-				myq.insert(stud);
-
-				//System.out.println(stud);
-				break;
-
-			default:
-				System.out.println("Input in wrong... \nEnter Correct Input.....! ");
 
 			}
-
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
 	}
 
 }

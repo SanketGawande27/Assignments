d038343531d295c2da57c1e0d447ffc9a02b37b4
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-08-03 17:32:38 +0530

    update studentclass

--- Queue/src/com/assignment/studentinfo/StudentInfo.java
+++ Queue/src/com/assignment/studentinfo/StudentInfo.java
@@ -8,7 +8,7 @@ public class StudentInfo<E> {
 	private float marks;
 	private Node obj, front, rear;
 
-	public StudentInfo(String name, String address, int rollno, float marks) {
+	public StudentInfo(String name, String address, int rollno, double marks) {
 		super();
 		this.name = name;
 		this.address = address;
@@ -51,7 +51,7 @@ public class StudentInfo<E> {
 		return marks;
 	}
 
-	public void setMarks(float marks) {
+	public void setMarks(double marks) {
 		this.marks = marks;
 	}
 
@@ -65,29 +65,3 @@ public class StudentInfo<E> {
 	}
 
 }
-
-/*
- * String result = ""; Node obj = front; while (obj != null) { result +=
- * obj.getobj(); result += ", ";
- * 
- * obj = obj.getNext(); } if (result == "") return "Empty...";
- * 
- * return "StudentInfo [name=" + name + ", rollno=" + rollno + ", marks=" +
- * marks + ", " + "Address =" + address + "]";
- * 
- */
-
-/*
- * // String result = ""; Node ptr = obj;while(ptr!=null) { // result +=
- * ptr.getobj(); System.out.println(ptr.getobj()); // if (ptr.getNext() != null)
- * { // result += ", "; // } // ptr = ptr.getNext();
- * 
- * }if(result=="")return"Queue is Empty ...! ";
- * 
- * return"StudentInfo [name="+name+", rollno="+rollno+", marks="+marks+", "
- * +"Address ="+address+"]";
- * 
- * // System.out.println(ptr.getobj()); }
- * 
- * }
- */
